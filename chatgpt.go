590a29c664ba748381500a14e4c67ab9d7b522a6
Rishabh Gupta <73791635+RGTechPro@users.noreply.github.com>
2023-02-21 21:51:51 +0530

    changed the prompts

--- chatgpt.go
+++ chatgpt.go
@@ -27,10 +27,10 @@ if scanner.Scan() {
 	ctx := context.Background()
 	client := gpt3.NewClient(apiKey)
 	
-	resp, err := client.CompletionWithEngine(ctx,"text-davinci-003", gpt3.CompletionRequest{
-		Prompt:    []string{question},
+	resp, err := client.CompletionWithEngine(ctx,"davinci", gpt3.CompletionRequest{
+		Prompt:    []string{"I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ:" + question+"?\nA:"},
 		MaxTokens: gpt3.IntPtr(100),
-		Stop:      []string{"."},
+		Stop:      []string{"\n"},
 		Echo:      false,
 		Temperature: gpt3.Float32Ptr(0),
 		TopP: gpt3.Float32Ptr(1),
